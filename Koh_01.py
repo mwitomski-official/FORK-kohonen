4cbbeb8b09586ec1c07dcb6231002f860fead2c8
3r3m <33702106+majkowskir@users.noreply.github.com>
2020-01-05 17:58:20 +0100

    Update Koh_01.py

--- Koh_01.py
+++ Koh_01.py
@@ -3,27 +3,36 @@ import numpy as np
 import random as rand
 
 path = 'obrazy/lena.png'
+#path = 'obrazy/black.png'
+#path = 'obrazy/black_3_white_squares.png'
+#path = 'obrazy/white.png'
+
 image = cv2.imread(path)
 
 height, width, channels = image.shape
 print(height, width, channels)
 
-dim = (512, 512)
-resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-cv2.imshow("", resized)
-(Blue, Green, Red) = cv2.split(resized)
-# cv2.imshow("Red", R)
-# cv2.imshow("Green", G)
-# cv2.imshow("Blue", B)
 
-frame = 2 # rozmiar pojedynczej ramki
-ile_blokow = int(((512/frame)**2))
-
+frame = 4 # rozmiar pojedynczej ramki
 ile_neuronow = 4
 ile_ramek_treningowych = 80
 
 # współczynnik uczenia
 eta = 0.01
+
+ile_blokow = int(((512/frame)**2))
+
+def normalizacja_wektora(input):
+	if sum(input.flatten()) != 0:
+		#return input.flatten()/sum(input.flatten())
+		return input/sum(input)
+	else:
+		return 0
+
+dim = (512, 512)
+resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+cv2.imshow("", resized)
+(Blue, Green, Red) = cv2.split(resized)
 
 print("Obraz podzielono na %s bloków"% ile_blokow)
 
@@ -32,57 +41,91 @@ for x in range(0, 511, frame):
 	for y in range(0, 511, frame):
 		puzzle.append(Red[x:x+frame,y:y+frame])	# ? .flatten()
 
-puzzle = np.array(puzzle)
-#print(puzzle[0])
+puzzle = np.array(puzzle).astype(float)
 
-def normalizacja_wektora(input):
-	if sum(input.flatten()) != 0:
-		return input.flatten()/sum(input.flatten())
-	else:
-		return 0
 
 # tutaj budujemy macierz wektorów normalizowanych, aby nie liczyć wielokrotnie - może się przyda?
-puzzle_norm = []
-for i in range(ile_blokow):
-	puzzle_norm.append(normalizacja_wektora(puzzle[i]))
+# puzzle_norm = []
+# for i in range(ile_blokow):
+# 	puzzle_norm.append(normalizacja_wektora(puzzle[i]))
 
 # otrzymujemy tablicę spłaszczonych wektorów normalizowanych puzzle_norm
 # budujemy macierz neuronów "startowych", czyli tak naprawdę losujemy indexy ramek z puzzle[]
 
 neurony = []
 for i in range(ile_neuronow):
-	r = rand.randint(0,	ile_blokow-1)
-	neurony.append(puzzle[r])
+	r = rand.randint(0,	ile_blokow-1) 
+	neurony.append(puzzle[r].flatten())
 
+neurony = np.array(neurony)
 print("Wyznaczono neurony: %s"% ile_neuronow)
+print(neurony)
+print(type(neurony))
+
 
 training_set = []
 for i in range(ile_ramek_treningowych):
 	r = rand.randint(0,ile_ramek_treningowych)
-	training_set.append(puzzle[r])
+	training_set.append(puzzle[r].flatten())
 
 print("Wyznaczono training set: %s"% ile_ramek_treningowych)
+training_set=np.array(training_set)
+print(training_set)
+print(type(training_set))
 
 # dla każdego neuronu znajdujemy najbliższą ramkę z training setu, wyznaczamy Best Matching Unit 
 
-
 for k in range(5):
-	BMU = []		
+	print("Krok %s"% k)
+	BMU = []
 	for i in range(ile_neuronow):
 		distance = []
 		for j in range(ile_ramek_treningowych):
 			#print(i,j)
 			# do porównania w locie normalizujemy wskazane wektory neuronu i ramki obrazu
-			odl = [i, j, sum(normalizacja_wektora(neurony[i])*(normalizacja_wektora((training_set[j]))))]
+			# odl = [i, j, sum(normalizacja_wektora(neurony[i])*(normalizacja_wektora((training_set[j]))))]
+			# print("Neuron")
+			# print(normalizacja_wektora(neurony[i]))
+			# print("Ramka")
+			# print(normalizacja_wektora((training_set[j])))
+			suma_wazona_wek = normalizacja_wektora(neurony[i]).dot(normalizacja_wektora(training_set[j]))
+			# print("Iloczyn")
+			# print(iloczyn_wek)
+			odl = [i, j, suma_wazona_wek]
 			distance.append(odl)
 		#distance = np.array(distance)
-		BMU.append((min(distance, key=lambda x: x[2])))		
-
+		BMU.append((min(distance, key=lambda x: x[2])))	
+	print("BMU")
+	print(type(BMU))
+	
 	for each in range(len(BMU)):
-		new_neuron = (neurony[BMU[each][0]] + eta * (neurony[BMU[each][0]]-puzzle[BMU[each][1]]))
+	 	#new_neuron = (neurony[BMU[each][0]] + eta * (neurony[BMU[each][0]]-puzzle[BMU[each][1]]))
+		print("stary neuron")
+		print(type(neurony[BMU[each][0]]))
+		print("ramka z training set")
+		print(training_set[BMU[each][1]])
+		new_neuron = neurony[BMU[each][0]] + eta * (neurony[BMU[each][0]] - training_set[BMU[each][1]])
+		print("nowy neuron")
+		print(new_neuron)
+		#new_neuron = np.array(new_neuron)
+#		cos = [1.2,2.3,3,4,5,6,7,8,9,0,1,2,3,4,5,6]
+#		new_neuron = new_neuron.tolist()
+#		print(type(new_neuron))
+		#print(cos)
+		print("Przypisanie do tablicy neuronów")
 		neurony[BMU[each][0]] = new_neuron
+		print(neurony[BMU[each][0]])
+
+	 	#print(neurony[BMU[each][0]])
+	 	#print(puzzle[BMU[each][1]])
+		
+	 	
+	 	
+	 	#print(neurony[BMU[0][0]])
 
 
+# array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15])
+# >>> A = A.reshape(4,4)
 
 # print("stary neuron")
 # print(neurony[BMU[0][0]])
